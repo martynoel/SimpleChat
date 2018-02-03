63de457e9d115498b3bc22fcf73cc143f524679b
Mimi Chenyao <mimichenyao@gmail.com>
2018-02-03 10:27:26 -0500

    Initial commit

---
+++ SimpleChat/SimpleChat/Controller/LoginController.swift
@@ -0,0 +1,48 @@
+//
+//  LoginController.swift
+//  SimpleChat
+//
+//  Created by Mimi Chenyao on 2/2/18.
+//  Copyright © 2018 Mimi Chenyao. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    
+    let inputsContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.white
+        
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor(r: 61, g: 91, b: 151)
+        view.addSubview(inputsContainerView)
+        setupInputsContainerView()
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    func setupInputsContainerView() {
+        // Need x, y, width, and height constraints for white box
+        inputsContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        inputsContainerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        inputsContainerView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
+        inputsContainerView.heightAnchor.constraint(equalToConstant: 150).isActive = true
+    }
+}
+
+extension UIColor {
+    
+    convenience init(r: CGFloat, g: CGFloat, b: CGFloat) {
+        self.init(red: r/255, green: g/255, blue: b/255, alpha: 1)
+    }
+}
